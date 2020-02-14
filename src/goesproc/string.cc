d53097abce57d5598bc5f13ee5992229de78c611
Sam <sam210723@hotmail.com>
2020-02-14 19:45:55 +1000

    Remove goesproc

--- src/goesproc/string.cc
+++
@@ -1,9 +0,0 @@
-#include "string.h"
-
-std::string removeSuffix(const std::string& str) {
-  auto pos = str.rfind('.');
-  if (pos != std::string::npos) {
-    return str.substr(0, pos);
-  }
-  return str;
-}
--- src/goesproc/string.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string removeSuffix(const std::string& str);
