8eca4e70650f495e45c0deb95dbab6eea856cf84
Sam <sam210723@hotmail.com>
2020-02-16 16:52:07 +1000

    Windows build script

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -1,6 +1,13 @@
+# Linux
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
 make
 sudo make install
+goesrecv --version
+
+# Windows
+rm -rf *
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_SYSTEM_NAME="Windows"
+make
 goesrecv --version
