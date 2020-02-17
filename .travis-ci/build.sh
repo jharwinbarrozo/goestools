2534f307c1d1a2418c6eac2db5bb6683c4d5f3e5
Sam <sam210723@hotmail.com>
2020-02-17 10:32:01 +1000

    Processor arch argument

--- .travis-ci/build.sh
+++ .travis-ci/build.sh
@@ -10,5 +10,5 @@ goesrecv --version
 rm -rf *
 printf '1\n\n' | sudo update-alternatives --config i686-w64-mingw32-g++
 printf '1\n\n' | sudo update-alternatives --config x86_64-w64-mingw32-g++
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_SYSTEM_NAME="Windows"
+cmake .. -DCMAKE_SYSTEM_NAME="Windows" -DCMAKE_SYSTEM_PROCESSOR="i686"
 make
